1b3b3b86f152df30d2390d8178557582853c9e6c
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-09-28 18:40:42 +0900

    use CustomerManagedS3

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ readonly PROJECT_NAME="aws_iot_analytics_sample"
 readonly TEMPLATE="`pwd`/template.yaml"
 readonly STACK_NAME="${PROJECT_NAME//_/-}-stack"
 
+readonly CHANNEL_BUCKET_NAME="${PROJECT_NAME//_/-}-iot-analytics-channel-bucket"
+# aws s3 mb "s3://${CHANNEL_BUCKET_NAME}"
+
 aws cloudformation validate-template \
   --template-body "file://${TEMPLATE}"
 
@@ -15,4 +18,5 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     NamePrefix=${PROJECT_NAME} \
-    IoTCertificateName=${AWS_IOT_CERTIFICATE_NAME}
+    IoTCertificateName=${AWS_IOT_CERTIFICATE_NAME} \
+    ChannelBucketName=${CHANNEL_BUCKET_NAME}
